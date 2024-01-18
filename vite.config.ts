23fb9071a98b17d4e977eec44660cede1d9109a9
Ryan Florence <rpflorence@gmail.com>
2024-01-18 12:35:26 -0700

    bLKAJSDFOIUASODF

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,21 @@ import {unstable_vitePlugin as remix} from '@remix-run/dev'
 import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import InSpeCt from 'vite-plugin-inspect'
 
 export default defineConfig({
   plugins: [
-    // react({
-    //   jsxImportSource: '@emotion/react',
-    //   babel: {
-    //     plugins: ['babel-plugin-macros', '@emotion/babel-plugin'],
-    //   },
-    // }),
-    remix({
-      appDirectory: 'src',
-      unstable_ssr: false,
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['babel-plugin-macros', '@emotion/babel-plugin'],
+      },
     }),
+    // remix({
+    //   appDirectory: 'src',
+    //   unstable_ssr: false,
+    // }),
     tsconfigPaths(),
+    InSpeCt(),
   ],
 })
