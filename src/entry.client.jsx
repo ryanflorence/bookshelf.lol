9b11d2310bcb44687618a0fd6f281ce9b1e8d5ed
Ryan Florence <rpflorence@gmail.com>
2024-02-06 13:56:59 -0700

    remove devtools

--- src/entry.client.jsx
+++ src/entry.client.jsx
@@ -1,8 +1,5 @@
 import './bootstrap'
-import {loadDevTools} from './dev-tools/load'
 import {RemixBrowser} from '@remix-run/react'
 import {hydrateRoot} from 'react-dom/client'
 
-loadDevTools(() => {
-  hydrateRoot(root, <RemixBrowser />)
-})
+hydrateRoot(root, <RemixBrowser />)
